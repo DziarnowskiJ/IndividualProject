d1cc99dc6e03c6912f2b0acaad2cae1d648b6a1f
DziarnowskiJ <jakub.dziarnowski@city.ac.uk>
2023-05-15 16:07:08 +0200

    Minor bugfix

--- client/src/helpers/DeckHandler.js
+++ client/src/helpers/DeckHandler.js
@@ -1,34 +1,4 @@
-import CardBack from './cards/CardBack';
-import CardFront from './cards/CardFront';
 import Card from './Card';
-
-// export default class DeckHandler {
-//     constructor(scene) {
-//         this.scene = scene;
-//         this.playerDeck = [];
-//         this.opponentDeck = [];
-//         this.playerHand = [];
-//         this.opponentHand = [];
-
-//         /** render a card on screen
-//          * 
-//          * @param {*} x x-location of card
-//          * @param {*} y y-location of card
-//          * @param {*} name [cardBack | cardFront] 
-//          * @param {*} type type of the cards (domain and value)
-//          * @returns 
-//          */
-//         this.dealCard = (x, y, name, type) => {
-//             let newCard;
-//             if (name === "cardBack") {
-//                 newCard = new CardBack(scene);
-//             } else {
-//                 newCard = new CardFront(scene, name);
-//             }
-//             return (newCard.render(x, y, type));
-//         }
-//     }
-// }
 
 export default class DeckHandler {
     constructor(scene) {
